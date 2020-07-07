09ec586b0999ec703f1f18e33f295b2d89823be7
unknown <t.adaniya@adawarp.com>
2020-07-07 17:54:39 +0900

    initial commit

---
+++ windows/LocalKCL/App.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::LocalKCL::implementation
+{
+    struct App : AppT<App>
+    {
+        App() noexcept;
+    };
+} // namespace winrt::LocalKCL::implementation
+
+
